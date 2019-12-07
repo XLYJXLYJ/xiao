82c3f07511e66b23555dd7571c916e8a7663dbd3
XLYJXLYJ <18050435245@163.com>
2019-12-07 18:08:12 +0800

    20191207

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="contain">
-      <img src="/static/images/girl1.jpg">
-      <img src="/static/images/girl2.jpg">
-      <img src="/static/images/girl3.jpg">
+      <img src="cloud://lynn-2opnz.6c79-lynn-2opnz/other/wave.gif">
+      <!-- <img src="/static/images/girl2.jpg">
+      <img src="/static/images/girl3.jpg"> -->
       <div class="middle-contain">
         <span>1</span>
         <span>1</span>
@@ -33,18 +33,18 @@ mounted() {
   // env是你云开发的环境id。
   let that = this
   const db = wx.cloud.database()
-  db.collection('list').add({
-    // data 字段表示需新增的 JSON 数据
-    data: {
-      // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
-      description: "learn cloud database",
-      due: new Date("2018-09-01"),
-    },
-    success: function(res) {
-      // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
-      console.log(res)
-    }
-  })
+  // db.collection('list').add({
+  //   // data 字段表示需新增的 JSON 数据
+  //   data: {
+  //     // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+  //     description: "learn cloud database",
+  //     due: new Date("2018-09-01"),
+  //   },
+  //   success: function(res) {
+  //     // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+  //     console.log(res)
+  //   }
+  // })
 
 
   console.log(db)
